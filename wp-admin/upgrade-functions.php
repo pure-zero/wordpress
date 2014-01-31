cdb6a1419bfaee26889b04f6974fd237ded666a0
John P. Bloch <johnpbloch@gmail.com>
2014-01-31 01:15:14 -0500

    Updating 2.3 branch head

--- wp-admin/upgrade-functions.php
+++ wp-admin/upgrade-functions.php
@@ -1,5 +1,4 @@
 <?php
 // Deprecated.  Use includes/upgrade.php.
-_deprecated_file( basename(__FILE__), '2.5', 'wp-admin/includes/upgrade.php' );
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 ?>
