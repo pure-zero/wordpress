69c9714cbe2a74a7cc49e16216cf9bfc0f5a5385
John P. Bloch <johnpbloch@gmail.com>
2014-02-04 02:15:11 -0500

    Updating 2.3 branch head

--- wp-admin/bookmarklet.php
+++ wp-admin/bookmarklet.php
@@ -51,10 +51,7 @@ if ( !empty($content) ) {
 <head>
 <title><?php bloginfo('name') ?> &rsaquo; Bookmarklet &#8212; WordPress</title>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php echo get_option('blog_charset'); ?>" />
-<?php
-wp_admin_css( 'css/global' );
-wp_admin_css();
-?>
+<?php wp_admin_css(); ?>
 
 <style type="text/css">
 <!--
